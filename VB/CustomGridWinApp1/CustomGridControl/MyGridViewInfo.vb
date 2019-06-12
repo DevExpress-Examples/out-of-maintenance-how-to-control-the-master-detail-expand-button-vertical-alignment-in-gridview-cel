3bf98258852a68f7fd5894ec25f326cafe72db74
Vladislav <vladislav.yurgens@devexpress.com>
2019-06-12 12:15:27 +0300

    Update MyGridViewInfo.vb

--- VB/CustomGridWinApp1/CustomGridControl/MyGridViewInfo.vb
+++ VB/CustomGridWinApp1/CustomGridControl/MyGridViewInfo.vb
@@ -15,26 +15,26 @@ Namespace CustomGridControl
 		Protected Overrides Function CalcRowCellDrawInfoCore(ByVal ri As GridDataRowInfo, ByVal ci As GridColumnInfoArgs, ByVal cell As GridCellInfo, ByVal nextColumn As GridColumnInfoArgs, ByVal calcEditInfo As Boolean, ByVal nextRow As GridRow, ByVal allowCache As Boolean) As GridCellInfo
 			Dim result As GridCellInfo
 			result = MyBase.CalcRowCellDrawInfoCore(ri, ci, cell, nextColumn, calcEditInfo, nextRow, allowCache)
-			If cell.CellButtonRect = Rectangle.Empty Then
-				Return cell
+			If result.CellButtonRect = Rectangle.Empty Then
+				Return result
 			End If
 
             Dim aView As MyGridView = TryCast(View, MyGridView)
             If aView Is Nothing Then
-                Return cell
+                Return result
             End If
 
 
             If aView.OptionsView.DetailButtonsVAlignment = VertAlignment.Top Then
-                cell.CellButtonRect.Y = cell.CellValueRect.Y + CellVertIndent + CellValueVIndent
-                Return cell
+                result.CellButtonRect.Y = result.CellValueRect.Y + CellVertIndent + CellValueVIndent
+                Return result
             End If
 
             If aView.OptionsView.DetailButtonsVAlignment = VertAlignment.Bottom Then
-                cell.CellButtonRect.Y = cell.CellValueRect.Bottom - cell.CellButtonRect.Height - CellVertIndent - CellValueVIndent
-                Return cell
+                result.CellButtonRect.Y = result.CellValueRect.Bottom - result.CellButtonRect.Height - CellVertIndent - CellValueVIndent
+                Return result
             End If
-            Return cell
+            Return result
 		End Function
 	End Class
 End Namespace
